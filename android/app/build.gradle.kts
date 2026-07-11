767caa9a759fa78207df8d14b8f5aabc622f154a
zli426491-byte <zli426491@gmail.com>
2026-07-11 17:23:31 +0800

    Enable Android BuildConfig for release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,6 +20,10 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
+    buildFeatures {
+        buildConfig = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
